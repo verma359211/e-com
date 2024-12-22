907f642ff1a19cc8d2d36e0ecb097a449d676dce
verma359211 <vermachandan2912003@gmail.com>
2024-12-23 00:45:07 +0530

    configured ShadCN and Aceternity UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/ui/mode-toggle";
+// import { ThreeDCardDemo } from "@/components/ui/test";
+import { ShootingStarsAndStarsBackgroundDemo } from "@/components/ui/test";
 
 export default function Home() {
   return (
-    <div className="flex w-screen h-screen justify-center items-center">
-      Hi
-   </div>
-  );
-}
+		<div className="flex w-screen h-screen justify-center items-center">
+			<Button>Press</Button>
+			<ModeToggle />
+			{/* <ThreeDCardDemo/> */}
+			<ShootingStarsAndStarsBackgroundDemo />
+		</div>
+	);
+} 
---
+++ src/components/ui/test.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+import { ShootingStars } from "@/components/ui/shooting-stars";
+import { StarsBackground } from "@/components/ui/stars-background";
+export function ShootingStarsAndStarsBackgroundDemo() {
+	return (
+		<div>
+			<ShootingStars />
+			<StarsBackground />
+		</div>
+	);
+}
